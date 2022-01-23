3caf61e0fd0b5fe5c43c4dd665f62250c2e2b441
sagar12345 <sagar.khadka2001@gmail.com>
2022-01-23 20:15:28 +0545

    backend added

---
+++ conn.session.sql
@@ -0,0 +1,51 @@
+--@Block
+CREATE TABLE newaccount(
+        id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        middlename VARCHAR(30) NOT NULL,
+        fathername VARCHAR(30) NOT NULL,
+        email VARCHAR(30) NOT NULL,
+        mobilenumber INT(12) NOT NULL,
+        dateofbirth DATE NOT NULL,
+        accountnumber INT(10) NOT NULL,
+        accountbalance INT(100) NOT NULL,
+        address TEXT NOT NULL,
+        city VARCHAR(30) NOT NULL,
+        district VARCHAR(30) NOT NULL,
+        pincode INT(10) NOT NULL,
+        state VARCHAR(30) NOT NULL,
+        gender CHARACTER(7) NOT NULL
+    
+);
+
+
+--@Block
+CREATE TABLE contact(
+   id INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
+   firstname VARCHAR(30) NOT NULL,
+   lastname VARCHAR(30) NOT NULL,
+   email VARCHAR(30) NOT NULL,
+   message TEXT NOT null,
+   reg_date TIMESTAMP NOT NULL current_timestamp() ON UPDATE current_timestamp()
+);
+
+CREATE TABLE verifiedaccount(
+        id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        middlename VARCHAR(30) NOT NULL,
+        fathername VARCHAR(30) NOT NULL,
+        email VARCHAR(30) NOT NULL,
+        mobilenumber INT(12) NOT NULL,
+        dateofbirth DATE NOT NULL,
+        accountnumber INT(10) NOT NULL,
+        accountbalance INT(100) NOT NULL,
+        address TEXT NOT NULL,
+        city VARCHAR(30) NOT NULL,
+        district VARCHAR(30) NOT NULL,
+        pincode INT(10) NOT NULL,
+        state VARCHAR(30) NOT NULL,
+        gender CHARACTER(7) NOT NULL
+    
+);
